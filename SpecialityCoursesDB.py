40e7e27bc436fa2a2c74b6d669aacf86c2a03fe1
nivkopo <nivko8746@gmail.com>
2023-06-09 17:37:17 +0300

    changed add course and get course methods

--- SpecialityCoursesDB.py
+++ SpecialityCoursesDB.py
@@ -3,20 +3,26 @@ class SpecialityCoursesDB:
 
     def __init__(self, name):
         self._name = name
-        self._courses = dict()      # key: course number, value: course object
+        self._must_courses = dict()      # key: course number, value: course object
+        self._choise_courses = dict()      # key: course number, value: course object
 
 
     def get_name(self):
         return self._name
 
     def add_course(self, course):
-        if course.get_speciality(self._name) is not None:
-            self._courses[course.get_number()] = course
+        if course.get_speciality(self._name) == "Must":
+            self._must_courses[course.get_number()] = course
+        elif course.get_speciality(self._name) == "Choise":
+            self._choise_courses[course.get_number()] = course
         
 
     def get_course(self, number):
-        if number in self._courses.keys():
-            return self._computers[number]
+        if number in self._must_courses.keys():
+            return self._must_courses[number]
+        
+        elif number in self._choise_courses.keys():
+            return self._choise_courses[number]
         else:
             print("Does not exist")
             return None
