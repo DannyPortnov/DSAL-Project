1a2311757e344f04d143d4d236815a2cb7797abb
danih <danih@DESKTOP-GCQI5FM>
2023-06-09 12:35:07 +0300

    Formatted the file and added test BEFORE RUN

--- Student.py
+++ Student.py
@@ -1,11 +1,12 @@
 from io import TextIOWrapper
 import regex as re
 
+
 class Student:
     "Student Object Implementation"
 
     def __init__(self, file_name, syllabus_db):
-    # def __init__(self, name, id, major, minor, general_points, sport_points):
+        # def __init__(self, name, id, major, minor, general_points, sport_points):
         self._file_name = file_name
         self._syllabus_db = syllabus_db
         self._name = None
@@ -16,7 +17,7 @@ class Student:
         self._sport_points = None
         self._courses = list()  # list of the courses the student took, useful for priniting
         self._invalid_courses = dict()  # key: course, value: why course is invalid
-    
+
     # def __init__(self, name, id):
     # # def __init__(self, name, id, major, minor, general_points, sport_points):
     #     self._name = name
@@ -36,26 +37,25 @@ class Student:
 
     def set_id(self, id):
         self._id = id
-    
+
     def set_major(self, major):
         self._major = major
 
     def set_minor(self, minor):
         self._minor = minor
-   
+
     def set_general_points(self, general_points):
         self._general_points = general_points
 
     def set_sport_points(self, sport_points):
         self._sport_points = sport_points
 
-
     # TODO: maybe change the list to another data structure
     # add a course object to the stduent's list of courses
+
     def add_course(self, course):
         self._courses.append(course)
         course.mark_as_done()
-
 
     def read_student_data(self):
         f = self._open_db()
@@ -82,6 +82,7 @@ class Student:
             else:
                 self._invalid_courses[course] = "Course's data does not match Syllabus"
 
+
 def extract_course_data_from_line(line):
     line = line.strip()  # Remove leading/trailing whitespaces
     # Use regex to extract the course number, credit, and name
@@ -94,6 +95,7 @@ def extract_course_data_from_line(line):
         print("Credit:", credit)
         print("Name:", name)
     return course_number, credit, name
+
 
 def extract_student_data_from_line(line):
     line = line.strip()  # Remove leading/trailing whitespaces
@@ -110,5 +112,13 @@ def ignore_comments(file_path: str):
     for line in lines:
         if not line.startswith("#"):
             yield line
-    
-    
+
+
+def test_ignoring_comments():
+    student = Student("student1.txt", None)
+    student.read_student_data()
+    pass
+
+
+if __name__ == "main":
+    test_ignoring_comments()
