79f978229fd95902e54299445a612c1af8ed2074
danih <danih@DESKTOP-GCQI5FM>
2023-06-10 13:47:10 +0300

    ADDED CONSTANTS, IMPLEMENTED PARRALLEL COURSES CHECK Constants include Specialities, Interships and errors read_student_data updates each course semester number Course's is_finished_properly returns bool and message if false Added test for reading a student file Naming is_done methods and variable in Course has changed to was_taken

--- Course.py
+++ Course.py
@@ -1,4 +1,5 @@
 from __future__ import annotations  # for self reference
+import Contants
 
 
 class Course:
@@ -7,7 +8,8 @@ class Course:
     # TODO: need to create str method to class course in order to print: number, points, name
 
     # def __init__(self, number, points, name, is_must, computers, signals, devices, pre_courses_list, parallel_course):
-    def __init__(self, number: int, name: str, points: float, is_must: str, pre_courses_list: list[Course], parallel_course: Course):
+    def __init__(self, number: int, name: str, points: float, is_must: str,
+                 pre_courses_list: list[Course] | None, parallel_course: Course | None):
         self._name = name
         self._number = number
         self._points = points
@@ -17,14 +19,26 @@ class Course:
         self._pre_courses = self._set_pre_courses(pre_courses_list)
         # self._specialties = self._set_specialties(computers, signals, devices)  # the specialties in which this course is available
         self._parallel_course = parallel_course
+        self._semester_num: int = None  # the semester in which this course was taken
         # in order to check if a student took a course or not- default value is false
-        self._is_done = False
+        self._was_taken = False
 
+    def get_semester_num(self):
+        return self._semester_num
+
+    def get_name(self):
+        return self._name
+
+    def set_semester_num(self, semester_num: int):
+        self._semester_num = semester_num
+
+    def get_parallel_course(self):
+        return self._parallel_course
     # set a pre_course, key = pre_course's Course object
 
-    def _set_pre_courses(self, pre_courses_list: list[Course]):
+    def _set_pre_courses(self, pre_courses_list: list[Course] | None):
         pre_courses = dict()
-        if len(pre_courses_list):
+        if pre_courses_list != None and len(pre_courses_list):
             for course in pre_courses_list:
                 pre_courses[course] = None
         return pre_courses
@@ -35,17 +49,17 @@ class Course:
             self._is_must = True
         self._is_must = False
 
-    # mark if a student took this course
-    def mark_as_done(self):
-        self._is_done = True
+    def mark_as_taken(self):
+        """Mark the course as taken"""
+        self._was_taken = True
 
     # returns the condition of a course: must or choise
     def is_mandatory(self):
         return self._is_must
 
-    # returns course's status, if it was taken or not
-    def is_done(self):
-        return self._is_done
+    def was_taken(self):
+        """Returns True if the student took the course, False otherwise"""
+        return self._was_taken
 
     # returns course's points
     def get_points(self):
@@ -55,12 +69,18 @@ class Course:
     def get_number(self):
         return self._number
 
-    # check if the pre courses are finished, this allows to determine if a course was finished properly
-    def is_finished_properly(self):
+    def is_finished_properly(self) -> tuple[bool, str | None]:
+        """Checks if all the pre courses and the parrallel course were taken, 
+        this allows to determine if a course was finished properly"""
         if len(self._pre_courses):
             for course in self._pre_courses.keys():
                 if course.is_done() is False:
-                    return False
+                    return False, Contants.format_pre_course_error(self, course)
+        # Check if the parallel course was taken in the same semester
+        if self._parallel_course is not None:
+            if self._parallel_course.was_taken() is False \
+                    or self._parallel_course.get_semester_num() != self._semester_num:
+                return False, Contants.format_parallel_course_error(self, self._parallel_course)
         return True
 
     # validate a course by checking it's points, name and number
@@ -68,6 +88,9 @@ class Course:
         if number == self._number and points == self._points and name == self._name:
             return True
         return False
+
+    def __str__(self):
+        return f"Course: {self._number}, {self._name}, {self._points} points"
 
 
 # function that converts a condition's value to an actual word
