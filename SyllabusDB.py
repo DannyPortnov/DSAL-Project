38741f0448f62cc645918b42e1d12cb55dfe3427
danih <danih@DESKTOP-GCQI5FM>
2023-06-10 10:43:13 +0300

    Formatting Using keyword arguments when initializing courses

--- SyllabusDB.py
+++ SyllabusDB.py
@@ -2,6 +2,7 @@ import Course
 import SpecialityCourse
 import SpecialityCoursesDB
 
+
 class SyllabusDB:
     "Syllabus Data base Object Implementation"
 
@@ -21,20 +22,19 @@ class SyllabusDB:
         self._general_points = 6
         self._sport_points = 1
 
-    
     # open the csv file and read only the first line- header
+
     def _open_db(self):
         f = open(self._file, "r", encoding="utf-8")
         header = f.readline().strip().split(',')
         return f, header
-    
 
     def get_general_points(self):
         return self._general_points
-    
+
     def get_total_points(self):
         return self._total_points
-    
+
     def get_sport_points(self):
         return self._sport_points
 
@@ -46,11 +46,11 @@ class SyllabusDB:
         elif self._devices.get_name() == speciality:
             return self._devices
 
-
     # get the amount of points for each course type, regarding the type of project
+
     def get_required_points(self, final_project):
         return self._mandatory_points[final_project], self._major_points[final_project], \
-                self._minor_points[final_project], self._external_points[final_project]
+            self._minor_points[final_project], self._external_points[final_project]
 
     # TODO: update DB creation to ignore the lines with the symbol: #
     def create_db(self):
@@ -60,51 +60,51 @@ class SyllabusDB:
             # number, points, name, is_must, computers, signals, devices, pre_courses_list, parallel_course = extract_line(d)
             # course = Course(number, name, points, is_must, computers, signals, devices, pre_courses_list, parallel_course)
             if line[4] == "חובה":
-                course = Course(int(line[1]), line[2], float(line[3]), line[4], get_pre_course_obj(line[8:12]), self.get_course_by_number(line[12]))
+                course = Course(number=int(line[1]), name=line[2], points=float(line[3]), is_must=line[4],
+                                pre_courses_list=get_pre_course_obj(line[8:12]),
+                                parallel_course=self.get_course_by_number(line[12]))
                 self._mandatory_courses[course.get_points()] = course
             else:
-                course = SpecialityCourse(int(line[1]), line[2], float(line[3]), line[4], line[5], line[6], line[7], get_pre_course_obj(line[8:12]), self.get_course_by_number(line[12]))
+                course = SpecialityCourse(
+                    number=int(line[1]), name=line[2], points=float(line[3]), is_must=line[4],
+                    computers=line[5], signals=line[6], devices=line[7],
+                    pre_courses_list=get_pre_course_obj(line[8:12]),
+                    parallel_course=self.get_course_by_number(line[12])
+                )
                 self._computers.add_course(course)
                 self._signals.add_course(course)
                 self._devices.add_course(course)
-
-
+        f.close()
 
     def get_course_by_number(self, number):
         if number in self._mandatory_courses.keys():
             return self._mandatory_courses[number]
         else:
             print("Does not exist")
-        
+
         course = self._computers.get_course(number)
         if course is not None:
             return course
-        
+
         course = self._signals.get_course(number)
         if course is not None:
             return course
-        
+
         course = self._devices.get_course(number)
         if course is not None:
             return course
-
-
-
 
     # def set_name(self, name):
     #     self._name = name
 
     # def set_id(self, id):
     #     self._id = id
-    
+
     # def set_major(self, major):
     #     self._major = major
 
     # def set_minor(self, minor):
     #     self._minor = minor
-   
-
-
 
     # # TODO: maybe change the list to another data structure
     # # add a course object to the stduent's list of courses
@@ -112,5 +112,7 @@ class SyllabusDB:
     #     self._courses.append(course)
 
 # need to get the pre_courses objects by using the pre courses list of strings
+
+
 def get_pre_course_obj(pre_courses_list):
     pass
