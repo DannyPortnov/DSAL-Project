a0007eaec20624561ae3bed77e30c802ba3dee68
danih <danih@DESKTOP-GCQI5FM>
2023-06-10 13:46:34 +0300

    Formatting

--- SpecialityCourse.py
+++ SpecialityCourse.py
@@ -1,12 +1,14 @@
-import Course
+from Course import Course
+
 
 class SpecialityCourse(Course):
     "Course Object Implementation"
 
     def __init__(self, number, name, points, is_must, computers, signals, devices, pre_courses_list, parallel_course):
-        
+
         super().__init__(self, number, name, points, is_must, pre_courses_list, parallel_course)
-        self._specialities = self._set_specialities(computers, signals, devices)  # the specialties in which this course is available
+        # the specialties in which this course is available
+        self._specialities = self._set_specialities(computers, signals, devices)
         # self._computers = number_to_condition(computers)
         # self._signals = number_to_condition(signals)
         # self._devices = number_to_condition(devices)
@@ -25,14 +27,16 @@ class SpecialityCourse(Course):
 
     # def get_computers(self):
     #     return self._computers
-    
+
     # def get_signals(self):
     #     return self._signals
-    
+
     # def get_devices(self):
     #     return self._devices
 
 # function that converts a condition's value to an actual word
+
+
 def number_to_condition(num):
     if num == 1:
         return "Must"
