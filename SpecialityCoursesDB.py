db52e513442a74d6e8c232930eb49365da1243a9
danih <danih@DESKTOP-GCQI5FM>
2023-06-19 12:13:43 +0300

    - Fixed add_course - Fixed type hints - Shortened get_course()

--- SpecialityCoursesDB.py
+++ SpecialityCoursesDB.py
@@ -1,5 +1,6 @@
 from Course import Course
 from Constants import *
+from SpecialityCourse import SpecialityCourse
 
 
 class SpecialityCoursesDB:
@@ -8,15 +9,15 @@ class SpecialityCoursesDB:
     def __init__(self, name: Speciality):
         self._name: Speciality = name
         # key: course number, value: course object
-        self._courses: dict[int, Course] = {}
+        self._courses: dict[int, SpecialityCourse] = {}
         # self._must_courses = dict()      # key: course number, value: course object
         # self._choise_courses = dict()      # key: course number, value: course object
 
-    def get_name(self):
+    def get_name(self) -> Speciality:
         return self._name
 
-    def add_course(self, course: Course):
-        if course.get_speciality(self._name) is None:
+    def add_course(self, course: SpecialityCourse) -> None:
+        if course.get_speciality_course_type(self._name) != SpecialityCourseType.NA:
             self._courses[course.get_number()] = course
 
         # if course.get_speciality(self._name) == SpecialityCourseType.MUST:
@@ -24,13 +25,5 @@ class SpecialityCoursesDB:
         # elif course.get_speciality(self._name) == "Choise":
         #     self._choise_courses[course.get_number()] = course
 
-    def get_course(self, number: int):
-        if number in self._courses.keys():
-            return self._courses[number]
-
-        # if number in self._must_courses.keys():
-        #     return self._must_courses[number]
-
-        # elif number in self._choise_courses.keys():
-        #     return self._choise_courses[number]
-        return None
+    def get_course(self, number: int) -> SpecialityCourse | None:
+        return self._courses.get(number)
