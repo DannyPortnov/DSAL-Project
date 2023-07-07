b9a3c42709c3514788035bc1ed5dc729c83616c6
danih <danih@DESKTOP-GCQI5FM>
2023-07-07 19:49:10 +0300

    Improved courses selection algorithm a bit: Type hints 2d array instead of 3d Added check in the end of the function to make sure (might be redundent)

--- TestStudent.py
+++ TestStudent.py
@@ -1,4 +1,7 @@
-def assign_courses(internship_type, specialities, credit_points, courses):
+from typing import List, Dict, Optional, Any
+
+def assign_courses(internship_type: str, specialities: List[str], credit_points: int,
+                    courses: List[Dict[str, Any]]) -> Dict[str, List[Dict[str, str]]]:
     # define the credit point requirements for each speciality based on the internship type
     if internship_type == 'industry':
         speciality_requirements = {'computers': 20, 'signals': 10, 'devices': 0}
@@ -8,56 +11,69 @@ def assign_courses(internship_type, specialities, credit_points, courses):
         speciality_requirements = {'computers': 0, 'signals': 0, 'devices': 0}
 
     # initialize the assignment of courses to specialities
-    assignment = {speciality: [] for speciality in specialities}
+    assignment:Dict[str, List[Dict[str, str]]] = {speciality: [] for speciality in specialities}
 
-    n = len(courses)
-    m = credit_points + 1
-    dp = [[[False for k in range(m)] for j in range(m)] for i in range(n+1)]
-    dp[0][0][0] = True
+    num_courses = len(courses)
+    max_credit_points = credit_points + 1
+    is_possible = [[False for j in range(max_credit_points)] for i in range(num_courses+1)]
+    is_possible[0][0] = True
 
-    for i in range(1, n+1):
-        for j in range(m):
-            for k in range(m):
-                if courses[i-1]['speciality'] == specialities[0] or courses[i-1]['speciality'] == 'shared':
-                    if j >= courses[i-1]['credit_points']:
-                        dp[i][j][k] |= dp[i-1][j-courses[i-1]['credit_points']][k]
-                if courses[i-1]['speciality'] == specialities[1] or courses[i-1]['speciality'] == 'shared':
-                    if k >= courses[i-1]['credit_points']:
-                        dp[i][j][k] |= dp[i-1][j][k-courses[i-1]['credit_points']]
-                dp[i][j][k] |= dp[i-1][j][k]
+    for i in range(1, num_courses+1):
+        for j in range(max_credit_points):
+            if courses[i-1]['speciality'] == specialities[0] or courses[i-1]['speciality'] == 'shared':
+                if j >= courses[i-1]['credit_points']:
+                    is_possible[i][j] |= is_possible[i-1][j-courses[i-1]['credit_points']]
+            if courses[i-1]['speciality'] == specialities[1] or courses[i-1]['speciality'] == 'shared':
+                k = credit_points - j
+                if k >= courses[i-1]['credit_points']:
+                    is_possible[i][j] |= is_possible[i-1][j]
+            is_possible[i][j] |= is_possible[i-1][j]
 
-    if not dp[n][speciality_requirements[specialities[0]]][speciality_requirements[specialities[1]]]:
-        return None
+    if not any(is_possible[num_courses][j] for j in range(speciality_requirements[specialities[0]], max_credit_points)):
+        raise ValueError('There is no possible assignment of courses to specialities')
 
-    i = n
-    j = speciality_requirements[specialities[0]]
-    k = speciality_requirements[specialities[1]]
+    i = num_courses
+    j = next(j for j in range(speciality_requirements[specialities[0]], max_credit_points) if is_possible[num_courses][j])
     while i > 0:
+        k = credit_points - j
         if courses[i-1]['speciality'] == specialities[0] or courses[i-1]['speciality'] == 'shared':
-            if j >= courses[i-1]['credit_points'] and dp[i-1][j-courses[i-1]['credit_points']][k]:
+            if j >= courses[i-1]['credit_points'] and is_possible[i-1][j-courses[i-1]['credit_points']]:
                 assignment[specialities[0]].append(courses[i-1])
                 j -= courses[i-1]['credit_points']
                 i -= 1
                 continue
         if courses[i-1]['speciality'] == specialities[1] or courses[i-1]['speciality'] == 'shared':
-            if k >= courses[i-1]['credit_points'] and dp[i-1][j][k-courses[i-1]['credit_points']]:
+            if k >= courses[i-1]['credit_points'] and is_possible[i-1][j]:
                 assignment[specialities[1]].append(courses[i-1])
-                k -= courses[i-1]['credit_points']
                 i -= 1
                 continue
         i -= 1
 
+    # check that the sum of credit points for each speciality equals the requirements
+    # Not sure it is necessary to check this, consider removing it later
+    for speciality in specialities:
+        if sum(course['credit_points'] for course in assignment[speciality]) != speciality_requirements[speciality]:
+            raise ValueError('The sum of credit points for each speciality should equal the requirements')
+
     return assignment
 
+# is_possible[i][j] represents whether it is possible to assign the first
+#  i courses such that the first speciality has j credit points.
+#  The second speciality’s credit points can be inferred from
+#  the total credit points and the first speciality’s credit points.
+
 # example usage
-courses = [{'name': 'Course A', 'credit_points': 5, 'speciality': 'shared'},
-           {'name': 'Course B', 'credit_points': 5, 'speciality': 'shared'},
-           {'name': 'Course C', 'credit_points': 5, 'speciality': 'shared'},
-           {'name': 'Course D', 'credit_points': 4, 'speciality': 'shared'},
-           {'name': 'Course D', 'credit_points': 3, 'speciality': 'shared'},
-           {'name': 'Course D', 'credit_points': 3, 'speciality': 'shared'},
-           {'name': 'Course D', 'credit_points': 2, 'speciality': 'shared'},
-           {'name': 'Course D', 'credit_points': 3, 'speciality': 'shared'}]
-assignment = assign_courses('industry', ['computers', 'signals'], 30, courses)
+courses = [{'credit_points': 5, 'speciality': 'shared'},
+           {'credit_points': 5, 'speciality': 'shared'},
+           {'credit_points': 5, 'speciality': 'shared'},
+           {'credit_points': 4, 'speciality': 'shared'},
+           {'credit_points': 3, 'speciality': 'shared'},
+           {'credit_points': 3, 'speciality': 'shared'},
+           {'credit_points': 2, 'speciality': 'shared'},
+           {'credit_points': 3, 'speciality': 'shared'}]
+choosen_intership = 'industry'
+assignment = assign_courses(choosen_intership, ['computers', 'signals'], 30, courses)
+
 print(assignment)
-#  O(n * m^2) time, where n is the number of courses and m is the maximum number of credit points.
+
+# O(n * m) time, where n is the number of courses and m is the maximum number of credit points.
