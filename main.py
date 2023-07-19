7b99eb74aacbfd179fb03879d20416250aefaeca
nivkopo <nivko8746@gmail.com>
2023-07-19 15:51:00 +0300

    Works Very Well!

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ def is_txt_file(file_path):
 
 
 if __name__ == "__main__":
-    # file_name = input("Please enter full name of student's file (with extension)\n")
-    # if is_txt_file(file_name):
-    #     run_program(file_name)  # valid student
-    #     print("Done! Check your folder for the output file")
-    # else:
-    #     print("The file does not have a .txt extension. Run the program again.")
+    file_name = input("Please enter full name of student's file (with extension):\n")
+    if is_txt_file(file_name):
+        run_program(file_name)  # valid student
+        print("Done! Check your folder for the output file")
+    else:
+        print("The file does not have a .txt extension. Run the program again.")
 
-    run_program("invalid major.txt")
+    # run_program("invalid major.txt")
     # run_program("invalid major.txt")
     # run_program("Student with 2 specialties but missing 1 required in minor.txt")
     # run_program("minor computers.txt")
